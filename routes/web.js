9b577e2f940a464227ed6a2338539e4aaf5ae3d7
tanyajha29 <jhatanya211@gmail.com>
2025-12-01 22:39:15 +0530

    (web): render tasks view with optional user

--- routes/web.js
+++ routes/web.js
@@ -1,10 +1,13 @@
+// routes/web.js
 const express = require('express');
 const router = express.Router();
 
-/* simple pages */
 router.get('/', (req, res) => res.render('index'));
 router.get('/register', (req, res) => res.render('register'));
 router.get('/login', (req, res) => res.render('login'));
-router.get('/tasks', (req, res) => res.render('tasks'));
+router.get('/tasks', (req, res) => {
+  // optionally pass user from server session if you have it
+  res.render('tasks', { user: null });
+});
 
 module.exports = router;
